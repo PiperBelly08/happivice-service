15fc093b34b41d63a97b53268b1216b8e7e54d5e
PiperBelly08 <hawarimunte@gmail.com>
2025-09-05 19:56:50 +0700

    Add Google authentication and user role management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->user();
+
+    $user = User::where('email', $googleUser->getEmail())->first();
+
+    if ($user) {
+        $user->update([
+            'google_id' => $googleUser->getId(),
+            'avatar'    => $googleUser->getAvatar(),
+        ]);
+    } else {
+        $user = User::create([
+            'name'      => $googleUser->getName(),
+            'email'     => $googleUser->getEmail(),
+            'google_id' => $googleUser->getId(),
+            'avatar'    => $googleUser->getAvatar(),
+            'password'  => bcrypt(str()->random(16)),
+        ]);
+
+        // Assign a default role when a new user is created
+        $user->assignRole('user');
+    }
+
+    // Optional: ensure returning user always has a role
+    if (! $user->hasRole('user')) {
+        $user->assignRole('user');
+    }
+
+    Auth::login($user);
+
+    return redirect('/home');
+});
+
